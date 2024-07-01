164e634cb42a5ffdb4a63302d461d30924affb28
emil.dueringer <s2210456008@fhooe.at>
2024-07-01 17:34:38 +0200

    Navigation fix

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,27 @@
-import { Stack } from "expo-router";
 import React from "react";
 import { ThemeProvider } from '@rneui/themed';
 import theme from "../../theme/theme";
+import { Tabs } from "expo-router";
+
 
 export default function TabsLayout() {
     return (
         <ThemeProvider theme={theme}>
-            <Stack>
-                <Stack.Screen name="index" options={{
+            <Tabs>
+                <Tabs.Screen name="index" options={{
                     headerTitle: "Home",
                     title: "Home"
                 }}/>
-                <Stack.Screen name="(bridges)" options={{
+                <Tabs.Screen name="(bridges)" options={{
                     headerShown:false
                 }}/>
-                <Stack.Screen name="(harbors)" options={{
+                <Tabs.Screen name="(harbors)" options={{
                     headerShown:false
                 }}/>
-                <Stack.Screen name="(boat)" options={{
+                <Tabs.Screen name="(boat)" options={{
                     headerShown:false
                 }}/>
-            </Stack>
+            </Tabs>
         </ThemeProvider>
     )
 }
